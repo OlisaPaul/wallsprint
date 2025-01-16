b232a9bb9266ab7caa6d4b1d2cd25d773072f623
Paul <odirahchukwumma28@gmail.com>
2025-01-16 13:54:12 +0100

    Refactor Portal model to use CloudinaryField for logo storage
    - Updated Portal and PortalContent models to replace ImageField with CloudinaryField for improved image handling.
    - Adjusted logo field to allow blank and null values for better flexibility in data entry.
    - Refreshed binary cache files to reflect changes in models.

--- store/models.py
+++ store/models.py
@@ -400,7 +400,7 @@ class FileTransfer(CommonFields):
 
 class Portal(models.Model):
     title = models.CharField(max_length=255)
-    logo = models.ImageField(upload_to='logos/', null=True)
+    logo = CloudinaryField('image', blank=True, null=True)
     copy_from_portal_id = models.IntegerField(null=True, blank=True)
     customers = models.ManyToManyField(
         Customer, blank=True, related_name='portals')
@@ -565,7 +565,7 @@ class PortalContent(models.Model):
         # through='PortalContentCatalog',
         related_name='portal_contents'
     )
-    logo = models.ImageField(upload_to='logos/', null=True)
+    logo = CloudinaryField('image', blank=True, null=True)
     payment_proof = models.BooleanField(default=False)
     order_history = models.BooleanField(default=False)
 
