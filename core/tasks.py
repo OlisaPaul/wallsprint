ffc26a4ab04f5d8dfbce9cd137a17db1c3f76ff4
Paul <odirahchukwumma28@gmail.com>
2025-02-04 21:43:18 +0100

    changed the html format to a html

--- core/tasks.py
+++ core/tasks.py
@@ -3,25 +3,23 @@ from django.core.mail import send_mail
 from django.conf import settings
 from .models import BlacklistedToken
 from datetime import datetime, timedelta
+from django.template.loader import render_to_string
 
 
 @shared_task
 def send_notification_email_task(staff_email, instance_name, request_type):
+    message = render_to_string('email/notification_email.html', {
+        'instance_name': instance_name,
+        'request_type': request_type,
+    })
+
     send_mail(
         subject=f'Customer Request for Service – {instance_name}',
-        message=f'Dear Team,\n\n'
-        f'We have received a request from \
-            {instance_name} regarding {request_type}. '
-        f'Please review the details below:\n\n'
-        f'Customer Name: {instance_name}\n'
-        f'Request Type: {request_type}\n'
-        f'Kindly take the necessary steps to assist them at your earliest convenience.\n\n'
-        f'Best regards,\n'
-        f'The WallsPrinting Team\n'
-        f'{settings.EMAIL_HOST_USER}',
+        message='',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=staff_email,
         fail_silently=False,
+        html_message=message,
     )
 
 
