9cd02d7147bbe5699815d720ce25c70e6762818e
Paul <odirahchukwumma28@gmail.com>
2025-02-11 11:07:15 +0100

    Add 'wallsprint-b1r6.onrender.com' to ALLOWED_HOSTS for deployment support

--- wallsprint/settings.py
+++ wallsprint/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wallsprint.onrender.com', '127.0.0.1', 'wallsprinting-prod-4d5f463b9011.herokuapp.com']
+ALLOWED_HOSTS = ['wallsprint.onrender.com', '127.0.0.1',
+                 'wallsprinting-prod-4d5f463b9011.herokuapp.com', 'wallsprint-b1r6.onrender.com']
 
 
 # Application definition
